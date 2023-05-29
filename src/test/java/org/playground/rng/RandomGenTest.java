0abf85bfb7ff931dca52c1582ba957d5b86ca51f
Ivan Atanasov <iv.atanasow@gmail.com>
2023-05-29 11:39:57 +0300

    add more unit tests

--- src/test/java/org/playground/rng/RandomGenTest.java
+++ src/test/java/org/playground/rng/RandomGenTest.java
@@ -5,7 +5,70 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RandomGenTest {
+
+    @Test
+    public void invalidInputTest(){
+        int[] randomNums = null;
+        float[] probabilities = new float[]{0.01f, 0.3f, 0.58f, 0.1f, 0.01f};
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->{
+            RandomGen randomGen = new RandomGen(randomNums, probabilities);
+        });
+
+        String expectedMessage = "RandomNums should not be null or empty";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void invalidInputTest_1(){
+        int[] randomNums = new int[]{-1, 0, 1, 2, 3};;
+        float[] probabilities = null;
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->{
+            RandomGen randomGen = new RandomGen(randomNums, probabilities);
+        });
+
+        String expectedMessage = "Probabilities should not be null or empty";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void invalidInputTest_2(){
+        int[] randomNums = new int[]{-1, 0};
+        float[] probabilities = new float[]{0.01f, 0.3f, 0.58f, 0.1f, 0.01f};
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->{
+            RandomGen randomGen = new RandomGen(randomNums, probabilities);
+        });
+
+        String expectedMessage = "Parameters size does not match";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void invalidInputTest_3(){
+        int[] randomNums = new int[]{-1, 0, 1, 2, 3};
+        float[] probabilities = new float[]{0.01f, 0.3f, -0.58f, 0.1f, 0.01f};
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->{
+            RandomGen randomGen = new RandomGen(randomNums, probabilities);
+        });
+
+        String expectedMessage = "Probability value cannot be less than zero";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 
     @Test
     public void statisticalTest() {
